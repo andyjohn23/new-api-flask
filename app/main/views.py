4fc453ff8de95b89fb748858b5d17cc54d65e691
andyjohn23 <andrewnick906@gmail.com>
2020-10-19 23:21:51 +0300

    updated files

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,7 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template,request,redirect,url_for
 from . import main
 from ..request import get_news, get_news_articles
 from ..models import News_Source, News_Articles
-
 
 @main.route('/')
 def index():
@@ -10,14 +9,14 @@ def index():
 	view root page function that returns the index the page and its data
 	"""
 	business_news = get_news("business")
-	sports_news = get_news("sports")
-	technology_news = get_news("technology")
-	entertainment_news = get_news("entertainment")
-	health_news = get_news("health")
+	sports_news= get_news("sports")
+	technology_news= get_news("technology")
+	entertainment_news= get_news("entertainment")
+	health_news=get_news("health")
 
 	return render_template('index.html',health=health_news,business=business_news,sports= sports_news,technology= technology_news,entertainment= entertainment_news)
 
-@main.route("/sources/<id>")
+@main.route("/news_sources/<id>")
 def articles(id):
     """
     the route page for news articles
@@ -25,4 +24,3 @@ def articles(id):
     article = get_news_articles(id)
 
     return render_template("article.html", article= article)
-
